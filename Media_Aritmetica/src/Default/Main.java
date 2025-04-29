fc67b4a6e1529315af4f49a1a701a1593b0eae74
CosmoSouza <lucascap9@gmail.com>
2025-04-29 01:24:38 -0300

    Update Main.java

--- Media_Aritmetica/src/Default/Main.java
+++ Media_Aritmetica/src/Default/Main.java
@@ -1,19 +1,24 @@
-package Default;
+package Inadimplencia;
 
 import java.util.Scanner;
 
-public class Main {
+public class Principal {
 
-  public static void main(String[] args) {
-	  Scanner sc = new Scanner(System.in);
-	double x, y, media;
-	
-	System.out.print("Digite o Primeiro numero: ");
-	x = sc.nextDouble();
-    System.out.print("Digite o Segundo numero: ");
-    y = sc.nextDouble();
-    media = (x + y) / 2.0;
-    System.out.println("Media = "+ media);
-    sc.close();
-     }
-  }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        double x, y, media;
+
+        System.out.print("Digite o primeiro número: ");
+        x = sc.nextDouble();
+
+        System.out.print("Digite o segundo número: ");
+        y = sc.nextDouble();
+
+        media = (x + y) / 2.0;
+
+        System.out.println("Média = " + media);
+
+        sc.close();
+    }
+}
